849dcc942541fb25814de528747fd0dd52268da6
Michael Little <bio595@gmail.com>
2013-09-13 19:50:29 +1200

    refactored wallpaper_downloader extensively. Prevented duplicate image downloads

--- wallpaper_download.py
+++ wallpaper_download.py
@@ -4,10 +4,13 @@ from threading import Thread
 
 class WallpaperDownloader(object):
 
-	def __init__(self):
-		self.download_dir = os.path.expanduser("~/Desktop/wallpaperdump/")
+	def __init__(self, subreddit="wallpaperdump", download_dir="~/Desktop/wallpaperdump/"):
+		if download_dir[-1] != "/":
+			download_dir += "/"
+		self.download_dir = os.path.expanduser(download_dir)
 		self.finished_downloading = False
 		self.download_progress = "0%"
+		self.subreddit = subreddit
 
 	def get_downloaded_album(self):
 		downloads = os.listdir(self.download_dir)
@@ -20,40 +23,28 @@ class WallpaperDownloader(object):
 		self.download_progress = "0%"
 		Thread(target=self.__do_download).start()
 
+	def __check_subreddit_exists(self):
+		http = httplib2.Http()
+		r, c = http.request("http://www.reddit.com/subreddits/search.json?q={0}&limit=1".format(self.subreddit), "GET")
+		j = json.loads(c)
+		return len(j['data']['children']) == 1
+
 	def get_download_progress(self):
 		return self.download_progress
 
 	def has_finished_downloading(self):
 		return self.finished_downloading
 
-	def __do_download(self):
 
-		def sortByUpvotes(post_x, post_y):
-			return post_y['data']['ups'] - post_x['data']['ups']
-
-
-		#Find an album on /r/wallpaperdump and download it
-		print "Querying reddit"
+	def __get_posts_from_subreddit(self):
 		http = httplib2.Http()
-		resp, content = http.request("http://www.reddit.com/r/wallpaperdump/new.json?limit=100", "GET")
+		resp, content = http.request("http://www.reddit.com/r/{0}/new.json?limit=100".format(self.subreddit), "GET")
 		posts = json.loads(content)['data']['children']
 		
 		#Remove NSFW posts and posts that aren't imgur albums
-		posts = [post for post in posts if not post['data']['over_18'] and post['data']['url'].startswith("http://imgur.com/a/") ]
+		return [post for post in posts if not post['data']['over_18'] and post['data']['url'].startswith("http://imgur.com/a/") ]
 
-
-		#Could select the album by random
-		#Sort by upvotes, get the highest rated post
-		#posts.sort(sortByUpvotes)
-		post_index = int(random.random() * len(posts) - 1)
-		album_id = posts[post_index]['data']['url'][len("http://imgur.com/a/"):]
-		print posts[post_index]['data']['title']
-		print "Album ID: " + album_id
-		#remove anchor to a specific image if it exists
-		if '#' in album_id:
-			album_id = album_id[:album_id.index('#')]
-
-
+	def __get_image_list_from_imgur(self, album_id):
 		print "Quering imgur"
 		#Create a new connection object for talking to imgur api
 		http = httplib2.Http(disable_ssl_certificate_validation=True)
@@ -61,19 +52,55 @@ class WallpaperDownloader(object):
 		url = "https://api.imgur.com/3/album/"+ album_id + "/images"
 		resp, content = http.request(url, "GET", headers=headers)
 
-		images = json.loads(content)['data']
+		return json.loads(content)['data']
 
+	def __download_image(self, image):
+		#check if we've already downloaded this image before
+		downloads = os.listdir(self.download_dir)
+
+		filename = image['link'][len("http://i.imgur.com/"):]
+		if not filename in downloads:
+			http = httplib2.Http()
+			resp, content = http.request(image['link'], "GET")			
+			with open(self.download_dir + filename, 'wb') as f:
+				f.write(content)
+		else:
+			print "Duplicate image: {0}".format(filename)
+
+	def __do_download(self):
+
+		#Find an album on /r/wallpaperdump and download it
+		print "Querying reddit"
+		
+		if not self.__check_subreddit_exists():
+			print "subreddit does not exist, sorry"
+			return
+
+		posts = self.__get_posts_from_subreddit()
+
+		#Select the album randomly
+		post_index = 0 #int(random.random() * len(posts) - 1)
+
+		#Find the album ID
+		album_id = posts[post_index]['data']['url'][len("http://imgur.com/a/"):]
+		print posts[post_index]['data']['title']
+		
+		#remove anchor to a specific image if it exists
+		if '#' in album_id:
+			album_id = album_id[:album_id.index('#')]
+
+		print "Album ID: " + album_id
+
+		images = self.__get_image_list_from_imgur(album_id)
+		print "Number of images in album: " + str(len(images))
+		
 		#make the wallpaperdump folder if doesnt already exist
 		if not os.path.exists(self.download_dir):
 			os.makedirs(self.download_dir)
 
-		print "Number of images in album: " + str(len(images))
 		#Download each item
 		for i, image in enumerate(images):
-
-			resp, content = http.request(image['link'], "GET")			
-			with open(self.download_dir + image['link'][len("http://i.imgur.com/"):], 'wb') as f:
-				f.write(content)
+			self.__download_image(image)
 			self.download_progress = str(float(i + 1) / len(images) * 100) + "%"
 			sys.stdout.write( "Downloading " + image['link'] + "\t" + self.download_progress + "\r")
 			sys.stdout.flush()
@@ -83,9 +110,8 @@ class WallpaperDownloader(object):
 		print "Finished downloading"
 
 
-
 def main():
-	wd = WallpaperDownloader()
+	wd = WallpaperDownloader("pics", "~/Desktop/test")
 	wd.download_album()
 
 if __name__ == '__main__':
